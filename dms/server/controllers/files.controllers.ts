74a58aeb801ff7f81af5b894970a0d726897f0c6
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2025-08-28 02:06:50 +0000

    update controller and response class

--- dms/server/controllers/files.controllers.ts
+++ dms/server/controllers/files.controllers.ts
@@ -1,23 +1,28 @@
 import { controllerGroup } from '.';
-import { MyServerResponse } from '../objects';
+import { createStoredDocument } from '../db';
+import { MyServerJSONResponse } from '../objects';
 
 controllerGroup.add('GetFiles', async () => {
-  return new MyServerResponse({ data: { root: '', items: [] } });
+  return new MyServerJSONResponse({ data: { root: '', items: [] } });
 });
 
 controllerGroup.add('UploadFile', async (ctx) => {
-  console.log(ctx.req.body);
-  console.log(ctx.req.files);
-
   if (Array.isArray(ctx.req.files)) {
     throw new Error('Files should be object');
   }
 
-  const uploadedFile = ctx.req.files?.['file'];
+  const uploadedFile = ctx.req.files?.['file']?.[0];
 
-  if (!uploadedFile?.[0]) {
+  if (!uploadedFile) {
     throw new Error('file is required');
   }
 
-  return new MyServerResponse({ data: {} });
+  await createStoredDocument({
+    name: uploadedFile.originalname,
+    path: uploadedFile.path,
+    mimetype: uploadedFile.mimetype,
+    owner: '',
+  });
+
+  return new MyServerJSONResponse({ data: {} });
 });
--- dms/server/objects/MyServerResponse.ts
+++ dms/server/objects/MyServerResponse.ts
@@ -1,9 +1,29 @@
+export type MyServerResponseArgs = {
+  status?: number;
+};
+
 export class MyServerResponse<T> {
   data: T;
   status: number;
 
-  constructor(data: T, args?: { status?: number }) {
+  constructor(data: T, args?: MyServerResponseArgs) {
     this.data = data;
-    this.status = args?.status || 200;
+    this.status = args?.status ?? 200;
+  }
+}
+
+export class MyServerJSONResponse<T> extends MyServerResponse<T> {}
+
+export type MyServerStreamResponseArgs = MyServerResponseArgs & {
+  contentType: string;
+};
+
+export class MyServerStreamResponse<T> extends MyServerResponse<T> {
+  contentType: string;
+
+  constructor(data: T, args?: MyServerStreamResponseArgs) {
+    super(data, args);
+
+    this.contentType = args?.contentType ?? '';
   }
 }
