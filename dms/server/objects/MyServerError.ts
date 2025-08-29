873c23b4cb4a0373f34d788535495fcb0fc13c0e
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2025-08-29 03:33:27 +0000

    add/update error classes

--- dms/server/objects/MyServerError.ts
+++ dms/server/objects/MyServerError.ts
@@ -1,1 +1,53 @@
-export class MyServerError {}
+import { HttpStatusCode } from 'axios';
+
+export abstract class MyServerError {
+  data: NonNullable<unknown>;
+  status: number;
+
+  constructor(data: NonNullable<unknown>, args: { status: number }) {
+    this.data = data;
+    this.status = args.status;
+  }
+}
+
+export abstract class MyServer5xxError extends MyServerError {
+  constructor(data: NonNullable<unknown>, args?: { status: number }) {
+    super(data, {
+      status: args?.status ?? HttpStatusCode.InternalServerError,
+    });
+  }
+}
+
+export class MyServerInternalError extends MyServer5xxError {}
+
+export abstract class MyServer4xxError extends MyServerError {
+  constructor(data: NonNullable<unknown>, args?: { status: number }) {
+    super(data, {
+      status: args?.status ?? HttpStatusCode.BadRequest,
+    });
+  }
+}
+
+export class MyServerBadRequestError extends MyServer4xxError {
+  constructor(data: NonNullable<unknown>) {
+    super(data, {
+      status: HttpStatusCode.BadRequest,
+    });
+  }
+}
+
+export class MyServerForbiddenError extends MyServer4xxError {
+  constructor(data: NonNullable<unknown>) {
+    super(data, {
+      status: HttpStatusCode.Forbidden,
+    });
+  }
+}
+
+export class MyServerConflictError extends MyServer4xxError {
+  constructor(data: NonNullable<unknown>) {
+    super(data, {
+      status: HttpStatusCode.Conflict,
+    });
+  }
+}
