a83b12b8d22eb6b863e9bd4e512eed3912887b16
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2025-09-02 01:20:37 +0000

    update UserSession model

--- dms/server/db/models/UserSession.ts
+++ dms/server/db/models/UserSession.ts
@@ -2,7 +2,7 @@ import { Schema, model, InferSchemaType } from 'mongoose';
 
 const UserSessionSchema = new Schema({
   token: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, required: true },
+  userId: { type: String, required: true },
 });
 
 export const UserSession = model('UserSession', UserSessionSchema);
