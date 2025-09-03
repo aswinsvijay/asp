01d050a6d5f5a8cc133c8db8a202b1bb03c3b98a
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2025-09-03 17:56:00 +0000

    add keyword validator

---
+++ dms/server/keywords/mongoId.ts
@@ -0,0 +1,24 @@
+import { FuncKeywordDefinition } from 'ajv';
+import { AnySchemaObject, DataValidationCxt } from 'ajv/dist/types';
+import mongoose from 'mongoose';
+
+export const mongoId: FuncKeywordDefinition = {
+  keyword: 'x-mongo-id',
+  type: 'string',
+  schemaType: 'boolean',
+  validate: (schema: boolean, data: string, _?: AnySchemaObject, dataCxt?: DataValidationCxt) => {
+    if (!schema) {
+      return true;
+    }
+
+    if (mongoose.Types.ObjectId.isValid(data)) {
+      return false;
+    }
+
+    if (dataCxt) {
+      dataCxt.parentData[dataCxt.parentDataProperty] = new mongoose.Types.ObjectId(data);
+    }
+
+    return true;
+  },
+};
