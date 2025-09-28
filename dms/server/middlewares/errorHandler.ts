511be7c1276d9efe3f7b90140439333095ca4156
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2025-09-28 20:42:01 +0000

    update errorHandler middleware

--- dms/server/middlewares/errorHandler.ts
+++ dms/server/middlewares/errorHandler.ts
@@ -1,10 +1,12 @@
 import { IMiddleware } from 'koa-router';
 
+const DEFAULT_ERROR_MESSAGE = 'Something broke!';
+
 export const errorHandler: IMiddleware = async (ctx, next) => {
   try {
     await next();
   } catch (err) {
     ctx.status = 500;
-    ctx.body = err instanceof Error ? err.message : 'Something broke!';
+    ctx.body = err instanceof Error ? err.message || DEFAULT_ERROR_MESSAGE : DEFAULT_ERROR_MESSAGE;
   }
 };
