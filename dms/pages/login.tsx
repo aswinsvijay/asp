9242d66da40387e382eb8403db46ead16dea1549
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2025-09-30 18:24:09 +0000

    initial setup login page

---
+++ dms/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+const Login: React.FC = () => {
+  return (
+    <>
+      <Button variant="contained">Login</Button>
+    </>
+  );
+};
+
+export default Login;
