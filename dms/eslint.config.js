dc04bce30e2e1b5e43e2e6aae86d847779307492
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2025-10-02 19:39:36 +0000

    update eslint

---
+++ dms/eslint.config.js
@@ -0,0 +1,17 @@
+// @ts-check
+/* eslint-disable */
+
+const { defineConfig } = require('eslint/config');
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: {},
+});
+
+module.exports = defineConfig([
+  ...compat.config({
+    extends: ['next/core-web-vitals', '@aswinsvijay/eslint-config'],
+    ignorePatterns: ['dist/**/*'],
+  }),
+]);
