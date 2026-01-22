5c6d7df49df2886255e215df4fb413c23d291a59
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2026-01-22 02:47:33 -0600

    add logout

--- dms/pages/dashboard.tsx
+++ dms/pages/dashboard.tsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
-import { SidebarComponent, DashboardViews } from '@/src/components';
-import { Box } from '@mui/material';
-import { Colors, Spacing } from '@/src/utils';
+import { SidebarComponent, DashboardViews, CustomIcon } from '@/src/components';
+import { Box, Button } from '@mui/material';
+import { Colors, Spacing, AuthUtils } from '@/src/utils';
+import { useRouter } from 'next/router';
 
 const Dashboard: React.FC = () => {
   const [currentView, setCurrentView] = useState<(typeof DashboardViews)[number]['label']>('Files');
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await AuthUtils.logout();
+      await router.push('/login');
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
 
   return (
     <Box display={'flex'} height={'100%'} gap={Spacing.SMALL}>
@@ -16,7 +27,26 @@ const Dashboard: React.FC = () => {
           }}
         />
       </Box>
-      <Box display={'flex'} flex={'1'} p={Spacing.SMALL}>
+      <Box display={'flex'} flex={'1'} flexDirection={'column'} p={Spacing.SMALL} gap={Spacing.SMALL}>
+        <Box
+          height={'40px'}
+          bgcolor={Colors.BACKGROUND}
+          borderRadius={Spacing.MEDIUM}
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'flex-end'}
+          px={Spacing.SMALL}
+        >
+          <Button
+            variant="outlined"
+            onClick={() => {
+              void handleLogout();
+            }}
+            size="small"
+          >
+            <CustomIcon name="Logout" />
+          </Button>
+        </Box>
         <Box flex={'1'} bgcolor={Colors.BACKGROUND} borderRadius={Spacing.MEDIUM}>
           {DashboardViews.map(({ label, Component }) => {
             return currentView === label && <Component key={label} />;
