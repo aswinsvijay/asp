f8db5e1fc228622667a81380511a6cc81d5bac0b
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2026-03-09 18:28:36 -0700

    fix icon component

--- dms/src/components/CustomIcon.component.tsx
+++ dms/src/components/CustomIcon.component.tsx
@@ -3,10 +3,18 @@ import type * as Icons from '@mui/icons-material';
 
 type IconName = keyof typeof Icons;
 
+const toSnakeCase = (value: string) => {
+  return value
+    .replace(/([a-z0-9])([A-Z])/g, '$1_$2')
+    .replace(/([A-Z]+)([A-Z][a-z])/g, '$1_$2')
+    .replace(/[\s-]+/g, '_')
+    .toLowerCase();
+};
+
 export const convertIconName = (name: IconName) => {
   switch (name) {
     default:
-      return name.toLowerCase();
+      return toSnakeCase(name);
   }
 };
 
