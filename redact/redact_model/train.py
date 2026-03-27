2794cc1d1a3ebaa472d0984df3c6760fc48c92fe
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2026-03-26 21:32:04 -0500

    conll2003 code for PII

--- redact/redact_model/train.py
+++ redact/redact_model/train.py
@@ -1,126 +1,92 @@
-from typing import Protocol, Any, Iterable, TypedDict
-import spacy
-from spacy.util import minibatch
-import random
-from faker import Faker
-import os
-import pathlib
+# !pip install datasets==3.6.0
+from datasets import DatasetDict, Dataset, load_dataset, concatenate_datasets
+import re
+from transformers import AutoTokenizer, AutoModelForTokenClassification, Trainer, TrainingArguments
+import pprint
 
-dir = os.path.dirname(os.path.realpath(__file__))
-MODEL_BASE_PATH = pathlib.Path(dir) / 'models'
+def multi_split(data: Dataset, name_share: list[tuple[str, int]]):
+  total = sum(share for _, share in name_share)
+  result: dict[str, Dataset] = {}
 
-class Annotations(TypedDict):
-    entities: list[tuple[int, int, str]]
+  remaining_data = data
+  for name, share in name_share:
+    fraction = share/total
+    is_last = share == total
 
-class FakeDataGenerator:
-    generator: Faker
+    if is_last:
+      split_data = DatasetDict({
+        'train': remaining_data,
+        'test': None
+      })
+    else:
+      split_data = remaining_data.train_test_split(train_size=fraction)
 
-    def __init__(self) -> None:
-        self.generator = Faker()
+    result[name] = split_data['train']
+    remaining_data = split_data['test']
+    total -= share
 
-    def generate_one(self) -> tuple[str, Annotations]:
-        name = self.generator.name()
-        address = self.generator.address().replace("\n", ", ")
-        phone = self.generator.phone_number()
-        ssn = self.generator.ssn()
-        card = self.generator.credit_card_number()
-        email = self.generator.email()
+  return DatasetDict(result)
 
-        sentence = (
-            f"{name} lives at {address}. Contact: {phone}, SSN: {ssn}, "
-            f"Card: {card}, Email: {email}"
-        )
+dataset = load_dataset("conll2003")
+dataset = concatenate_datasets(dataset.values()).shuffle()
 
-        entities: list[tuple[int, int, str]] = []
-        for pii, label in [(name, "PERSON"), (address, "ADDRESS"), (phone, "PHONE"),
-                        (ssn, "SSN"), (card, "CREDIT_CARD"), (email, "EMAIL")]:
-            start = sentence.find(pii)
+dataset = multi_split(dataset, [['train', 6], ['validation', 2], ['test', 2]])
+dataset
 
-            if start < 0:
-                continue
+label_list = dataset["train"].features["ner_tags"].feature.names
+label_list
 
-            entities.append((start, start + len(pii), label))
+def cleanup_name(name: str):
+  regex = '[^A-Za-z0-9]+'
+  return re.sub(regex, '_', name)
 
-        return sentence, {"entities": entities}
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 
-    def generate(self, size: int):
-        return list(self.generate_one() for _ in range(size))
+def tokenize_and_align_labels(examples):
+    tokenized_inputs = tokenizer(
+        list(examples["tokens"]),
+        truncation=True,
+        padding="max_length",
+        is_split_into_words=True,
+        max_length=128
+    )
 
-class Model(Protocol):
-    name: str
+    labels = []
+    for i, label in enumerate(examples[f"{'ner'}_tags"]):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)
+        label_ids = []
+        for word_idx in word_ids:
+            if word_idx is None:
+                label_ids.append(-100)
+            elif label[word_idx] == '0':
+                label_ids.append(0)
+            else:
+                label_ids.append(label[word_idx])
+        labels.append(label_ids)
 
-    @property
-    def effective_path(self):
-        return MODEL_BASE_PATH / self.name
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs
 
-    def load_dataset(self) -> None:
-        ...
-    
-    def train(self) -> None:
-        ...
-    
-    def run(self, text: str) -> Any:
-        ...
+tokenized_datasets = dataset.map(tokenize_and_align_labels, batched=True)
 
-    def save_model(self):
-        ...
+model = AutoModelForTokenClassification.from_pretrained("bert-base-cased", num_labels=len(label_list))
 
-    def load_model(self):
-        ...
+args = TrainingArguments(
+    output_dir="./pii-model",
+    save_strategy="epoch",
+    learning_rate=2e-5,
+    num_train_epochs=3,
+    weight_decay=0.01,
+)
 
-class SpacyModel(Model):
-    name: str = 'spacy'
-    dataset: list[tuple[str, Annotations]]
+trainer = Trainer(
+    model=model,
+    args=args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    processing_class=tokenizer,
+)
 
-    def __init__(self) -> None:
-        self.nlp = spacy.blank("en")
-        self.ner = self.nlp.add_pipe("ner")
-        self.dataset = []
+trainer.train()
 
-    def load_dataset(self) -> None:
-        self.dataset = FakeDataGenerator().generate(1000)
-
-        for _, annotations in self.dataset:
-            for _, _, label in annotations["entities"]:
-                self.ner.add_label(label)
-
-    def train(self) -> None:
-        self.nlp.begin_training() # type: ignore
-
-        for i in range(10):
-            random.shuffle(self.dataset)
-            losses: dict[str, float] = {}
-            batches: Iterable[list[tuple[str, Annotations]]] = minibatch(self.dataset, size=20)
-            for batch in batches:
-                examples = []
-                for text, ann in batch:
-                    doc = self.nlp.make_doc(text)
-                    example = spacy.training.Example.from_dict(doc, ann)
-                    examples.append(example)
-                self.nlp.update(examples, drop=0.2, losses=losses)
-            print(f"Iteration {i + 1} - Loss {losses}")
-
-    def run(self, text: str):
-        return self.nlp(text)
-
-    def save_model(self):
-        self.nlp.to_disk(self.effective_path)
-
-    def load_model(self):
-        self.nlp = spacy.load(self.effective_path)
-        self.ner = self.nlp.get_pipe("ner")
-
-    def redact(self, text: str):
-        doc = self.nlp(text)
-        for ent in doc.ents:
-            if ent.label_ in ["PERSON", "ADDRESS", "PHONE", "SSN", "CREDIT_CARD", "EMAIL"]:
-                text = text.replace(ent.text, f"[{ent.label_}]")
-
-        return text
-
-if __name__ == "__main__":
-    model = SpacyModel()
-
-    model.load_dataset()
-    model.train()
-    model.save_model()
+trainer.evaluate()
