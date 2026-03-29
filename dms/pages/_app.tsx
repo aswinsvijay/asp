758cc14e0981ba5ca081a923b0e3bf9164f36a95
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2026-03-29 00:30:24 -0500

    setup for toast message

--- dms/pages/_app.tsx
+++ dms/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { SnackbarProvider } from 'notistack';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SnackbarProvider anchorOrigin={{ horizontal: 'right', vertical: 'top' }} maxSnack={3}>
+      <Component {...pageProps} />
+    </SnackbarProvider>
+  );
 }
