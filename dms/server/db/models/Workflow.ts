6989a5edd8e5299600e92408385bb023bc12c901
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2026-03-29 23:59:49 -0500

    update workflow schema

--- dms/server/db/models/Workflow.ts
+++ dms/server/db/models/Workflow.ts
@@ -1,21 +1,26 @@
-import { Schema, model, InferSchemaType, Require_id } from 'mongoose';
+import { Schema, model, Require_id, InferRawDocType } from 'mongoose';
 
-const WorkflowSchema = new Schema({
+const WorkflowSchemaDefinition = {
   name: { type: String, required: true },
   owner: { type: Schema.Types.ObjectId, required: true },
   url: { type: String, required: true },
-  inputs: [
-    {
-      name: { type: String, required: true },
-      type: {
-        type: String,
-        required: true,
-        enum: ['string', 'number', 'boolean', 'date'], // Only allow these specific types
+  inputs: {
+    type: [
+      {
+        name: { type: String, required: true },
+        type: {
+          type: String,
+          required: true,
+          enum: ['string', 'number', 'boolean', 'date'], // Only allow these specific types
+        },
       },
-    },
-  ],
-});
+    ],
+    required: true,
+  },
+} as const;
+
+const WorkflowSchema = new Schema(WorkflowSchemaDefinition);
 
 export const Workflow = model('Workflow', WorkflowSchema);
 
-export type Workflow = Require_id<InferSchemaType<typeof WorkflowSchema>>;
+export type Workflow = Require_id<InferRawDocType<typeof WorkflowSchemaDefinition>>;
